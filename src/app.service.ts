578b6124a666739a48885d7dde1e72aff29ca848
Dimitri Allegoet <dallegoet@gmail.com>
2023-10-06 22:33:49 +0200

    test

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import path from 'path';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  private session: any;
+
+  constructor() {
+    import('node-llama-cpp').then((nodeLLamaCpp) => {
+      const model = new nodeLLamaCpp.LlamaModel({
+        modelPath: path.join(__dirname, 'models', 'codellama-13b.Q3_K_M.gguf'),
+      });
+      const context = new nodeLLamaCpp.LlamaContext({ model });
+
+      this.session = new nodeLLamaCpp.LlamaChatSession({ context });
+    });
+  }
+
+  async getHello(): Promise<string> {
+    if (!this.session || !this.session.initialized) {
+      throw new Error('Session not initialized');
+    }
+
+    return await this.session.prompt('qui est tu ?');
   }
 }
